f02920436914f7bdeded85e014a3d20501b03473
SoniaBastos <soniacris.bastos@gmail.com>
2023-02-09 17:42:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const app = express();
-app.get('/', (req,res)=> {
-    res.status(200), send({message:'olá mundo estou funcionando'});
+app.get('/ola', (req,res)=> {
+    res.status(200), send({message:'oi eu sou a rota ola'});
 })
 app.listen(3001,() => {
     console.log('Api rodando na porta 3001');
